ca79c68e93e49b6106eb0719d3381d3ac3c536ef
svv0003 <ssetoo4689@naver.com>
2025-07-24 11:12:12 +0900

    switch문_커피 주문 기능 실습

--- js/05_조건문_switch.js
+++ js/05_조건문_switch.js
@@ -32,42 +32,45 @@ function 계절체크기능() {
 function 커피주문기능() {
   const coffee = document.getElementById("coffeeSelect").value;
   const coResult = document.getElementById("coffeeResult");
-  let price;
+  let menu, price, description;
   switch (coffee) {
     case "americano":
-      menu = "아메리카노";
+      menu = coffee;
       price = 2000;
-      설명 = "고소한 맛";
+      description = "고소한 맛";
       break;
     case "latte":
-      menu = "라떼";
+      menu = coffee;
       price = 3500;
-      설명 = "부드러운 맛";
+      description = "부드러운 맛";
       break;
     case "cappuccino":
-      menu = "카푸치노";
+      menu = coffee;
       price = 3800;
-      설명 = "달콤한 맛";
+      description = "달콤한 맛";
       break;
     case "mocha":
-      menu = "모카";
+      menu = coffee;
       price = 4200;
-      설명 = "진한 초콜릿 맛";
+      description = "진한 초콜릿 맛";
       break;
     case "macchiato":
-      menu = "마키아토";
+      menu = coffee;
       price = 4500;
-      설명 = "오묘한 맛";
+      description = "오묘한 맛";
       break;
     default:
-      coResult.innerHTML = "메뉴를 선택하세요";
+      menu = "메뉴를 선택하세요";
+      price = 0;
+      description = "메뉴가 선택되지 않았습니다"
   }
   // ${} : 템플릿 리터럴  (변수 삽입 문법)
   //       ES6부터 제공되는 문법
   //       ``(백틱, 역따옴표) 안에 사용하고자 하는 변수를 작성할 때 ${} 내부에 변수명 작성
   // ex)   coResult.innerHTML=`${price}`;
   // ex)   coResult.innerHTML=`주문하신 메뉴는 ${price}원이고, ${설명}이 특징입니다.`;
-  coResult.innerHTML = `<pre>메뉴 : ${menu}
+  coResult.innerHTML =
+`<pre>메뉴 : ${menu}
 가격 : ${price}원
-설명 : ${설명}이 특징입니다.</pre>`;
+설명 : ${description}</pre>`;
 }
