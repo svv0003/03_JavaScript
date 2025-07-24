0763655745fdc4c1a505f0bb4aee9751255db8c6
svv0003 <ssetoo4689@naver.com>
2025-07-24 16:10:21 +0900

    카카오톡 채팅창 생성

--- js/04_반복문.js
+++ js/04_반복문.js
@@ -1,18 +1,18 @@
 function check1() {
-  for (let num1=1; num1<=10; num1++){
+  for (let num1 = 1; num1 <= 10; num1++) {
     document.getElementById("확인1").innerHTML += num1 + " ";
   }
 }
 
 function check2() {
-  for (let num2=5; num2<=10; num2++){
+  for (let num2 = 5; num2 <= 10; num2++) {
     document.getElementById("확인2").innerHTML += num2 + " ";
   }
 }
 
-let sum3 = 0;
 function check3() {
-  for (let num3=1; num3<=10; num3++){
+  let sum3 = 0;
+  for (let num3 = 1; num3 <= 10; num3++) {
     sum3 += num3;
   }
   document.getElementById("확인3").innerHTML = sum3;
--- js/04_반복문실습_while문2.js
+++ js/04_반복문실습_while문2.js
@@ -21,3 +21,4 @@ function exit() {
     document.getElementById("choice").value;
 }
 
+document.get
