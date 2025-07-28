22d992f17c0e62f130a94b35fdf83dc26093f88c
svv0003 <ssetoo4689@naver.com>
2025-07-28 16:09:36 +0900

    정규표현식을 이용한 회원가입 실습

--- js/16_회원가입.js
+++ js/16_회원가입.js
@@ -25,13 +25,12 @@ const regExpName = /^[가-힣]{2,15}$/;
 const regExpPw = /^[A-Za-z\d!@#$%^&*]{8,20}$/;
 const gender = document.querySelector("[name='gender']:checked");
 
-
 inputId.addEventListener("input", (e) => {
   const valId = e.target.value;
 
   if (valId.trim().length == 0) {
     idResult.textContent = "영어, 숫자, -, _ 6~16글자 작성하세요.";
-    e.target.addEventListener = "";
+    e.target.value = "";
     idResult.classList.remove("check", "error");
     checkList["inputId"] = false;
     return;
@@ -50,50 +49,111 @@ inputId.addEventListener("input", (e) => {
   }
 });
 
-
 inputPw.addEventListener("input", (e) => {
   const valPw = e.target.value;
 
   if (valPw.trim().length == 0) {
-    pwResult.textContent = "영어 대소문자, 숫자, 특수문자(!@#$%^&*) 포함 8~20글자 작성하세요.";
-    e.target.addEventListener = "";
+    pwResult.textContent =
+      "영어 대소문자, 숫자, 특수문자(!@#$%^&*) 포함 8~20글자 작성하세요.";
+    e.target.value = "";
     pwResult.classList.remove("check", "error");
     checkList["inputPw"] = false;
+    checkPw기능();
     return;
   }
-    if (regExpPw.test(valPw)) {
+  if (regExpPw.test(valPw)) {
     pwResult.textContent = "유효한 비밀번호 형식입니다.";
     pwResult.classList.add("check");
     pwResult.classList.remove("error");
-    checkList["inputPw"] = true;            // 비밀번호 형식이 일치한다면 위 작성한 checkList에서 inputPw에 해당하는 boolean을 true로 변경하겠다.
+    checkList["inputPw"] = true; // 비밀번호 형식이 일치한다면 위 작성한 checkList에서 inputPw에 해당하는 boolean을 true로 변경하겠다.
   } else {
     pwResult.textContent = "유효하지 않은 비밀번호 형식입니다.";
     pwResult.classList.add("error");
     pwResult.classList.remove("check");
     checkList["inputPw"] = false;
   }
+  checkPw기능();
 });
 
+inputPwCheck.addEventListener("input", checkPw기능);
 
-inputPwCheck.addEventListener("input", (e) => {
-  const valPwCheck = e.target.value;
-
-  if (valPwCheck.trim().length == 0) {
-    pwCheckResult.textContent = "비밀번호를 한 번 더 작성하세요.";
-    e.target.addEventListener = "";
+function checkPw기능() {
+  // trim() : 문자열 좌우에서 공백을 제거하는 메소드(함수=기능)
+  const pwVal = inputPw.value.trim();
+  const pwCheckVal = inputPwCheck.value.trim();
+  if (pwVal.length == 0 || pwCheckVal.length == 0) {
+    pwCheckResult.textContent = "비밀번호를 먼저 입력해주세요.";
+    // pwCheckResult.style.background = "배경색";    2순위 형식의 style css
+    // pwCheckResult의 class 명칭을 추가하거나 제거하겠다. 4순위 style css
+    // 태그 명칭 옆에 class 명칭을 지우겠다. check와 error를 제거하겠다.
     pwCheckResult.classList.remove("check", "error");
     checkList["inputPwCheck"] = false;
     return;
   }
-    if (regExpPw.test(valPwCheck)) {
-    pwCheckResult.textContent = "유효한 비밀번호 형식입니다.";
+
+  if (pwVal == pwCheckVal) {
+    pwCheckResult.textContent = "비밀번호가 일치합니다.";
     pwCheckResult.classList.add("check");
     pwCheckResult.classList.remove("error");
     checkList["inputPwCheck"] = true;
   } else {
-    pwCheckResult.textContent = "유효하지 않은 비밀번호 형식입니다.";
+    pwCheckResult.textContent = "비밀번호가 일치하지 않습니다.";
     pwCheckResult.classList.add("error");
     pwCheckResult.classList.remove("check");
     checkList["inputPwCheck"] = false;
   }
+}
+
+inputName.addEventListener("input", (e) => {
+  const valName = e.target.value;
+
+  if (valName.trim().length == 0) {
+    nameResult.textContent = "한글 2~15글자 작성하세요.";
+    e.target.value = "";
+    nameResult.classList.remove("check", "error");
+    checkList["inputName"] = false;
+    return; // 제대로 작성 안 했으니까 아래 항목을 사용할 수 없도록 돌려보내기 설정
+  }
+  if (regExpName.test(valName)) {
+    nameResult.textContent = "유효한 이름 형식입니다.";
+    nameResult.classList.add("check");
+    nameResult.classList.remove("error");
+    checkList["inputName"] = true;
+  } else {
+    nameResult.textContent = "유효하지 않은 이름 형식입니다.";
+    nameResult.classList.add("error");
+    nameResult.classList.remove("check");
+    checkList["inputName"] = false;
+  }
+});
+
+btn.addEventListener("click", (e) => {
+  if (checkList.inputId == false) {
+    alert("아이디가 유효하지 않습니다.");
+    inputId.focus();
+    return;
+  } else if (checkList.inputPw == false) {
+    alert("비밀번호가 유효하지 않습니다.");
+    inputPw.focus();
+    return;
+  } else if (checkList.inputPwCheck == false) {
+    alert("비밀번호 확인이 유효하지 않습니다.");
+    inputPwCheck.focus();
+    return;
+  } else if (checkList.inputName == false) {
+    alert("이름이 유효하지 않습니다.");
+    inputName.focus();
+    return;
+  }
+
+  // 회원가입 버튼을 클릭했을 때 성별 체크를 했는지 확인하도록 gender 위치를 버튼 내에 작성
+  const gender = document.querySelector("[name='gender']:checked");
+  if (gender == null) {
+    alert("성별을 선택해주세요");
+    return;
+  }
+
+  // 모든게 true, 회원가입이 완료되면
+  alert("회원가입이 완료되었습니다.");
+  signupForm.submit(); // 회원가입 폼 제출 완료 기능 설정
 });
