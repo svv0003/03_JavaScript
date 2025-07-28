d3732ca03d4e230a631be8ef36bf89b2b3645036
svv0003 <ssetoo4689@naver.com>
2025-07-28 11:20:10 +0900

    함수 실습(화살표 함수)

--- js/13_함수.js
+++ js/13_함수.js
@@ -46,3 +46,63 @@ btn1.addEventListener("click", function () {
 });
 
 
+const btn3a = document.getElementById("btn3a");
+btn3a.addEventListener("click", function(){
+  const numbers = [];
+
+  numbers[0]=30;
+  numbers[2]=10;
+  console.log(numbers);
+
+  // 배열.push(값)  : 배열의 마지막 요소로 값을 추가하는 방식
+  numbers.push(40);       // index 2번 째 자리 다음에 40이 추가됨
+  console.log(numbers);
+
+  // pow(a,b)     : a의 b제곱
+  numbers.push(pow(2,4)); // ==numbers.push(16);
+  // push 또는 index[idx]를 이용해서 값을 추가할 때 함수로 만든 기능을 활용해서 숫자나 글자 추가할 수 있다.
+})
+
+// 전달 받은 수를 x제곱해서 반환하는 방식 (=pow 기능을 표현한 for문)
+function pow(num, x) {
+  let result = 1;     // 곱했을 때 결과에 영향을 주지 않기 위해 1 초기화
+
+  for(let i=0; i<x; i++){
+    result *= num;
+  }
+  return result;
+}
+
+
+// 화살표 함수
+const arrowList = document.querySelectorAll(".arrow");
+console.log("arrowList 확인 : ", arrowList);
+
+// 화살표 함수 기본 형태
+// arrowList[0].addEventListener("click", function(){})
+// ↓
+arrowList[0].addEventListener("click", () => {
+  alert("화살표 함수 기본 형태 실습");
+})
+
+// 매개 변수가 1개인 경우 () 생략 가능
+// arrowList[0].addEventListener("click", function(e){})
+// ↓
+arrowList[1].addEventListener("click", e => {
+  // e        : 이벤트 객체(모든 이벤트 관련 정보가 담겨있는 객체)
+  // e.target : 이벤트가 발생한 요소
+  e.target.style.backgroundColor = "hotpink";
+});
+
+// return 한 줄인 경우 {} 생략 가능
+// arrowList[0].addEventListener("click", function(e){})
+// ↓
+arrowList[2].addEventListener("click", e => {
+  const input1 = Number(prompt("첫 번째 값 "));
+  const input2 = Number(prompt("두 번째 값 "));
+  alert("매개 변수가 1개인 경우", input1 + input2);})
+
+// return 한 줄인데 object 반환하는 경우 {} 생략 불가
+// arrowList[0].addEventListener("click", function(e){})
+// ↓
+arrowList[3].addEventListener("click", e => alert("매개 변수가 1개인 경우"))
