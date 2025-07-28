1b9f0099a7e4b4e3d9e3a9c557aef72bbe98058e
svv0003 <ssetoo4689@naver.com>
2025-07-28 12:34:53 +0900

    함수, 익명함수를 활용한 클릭 숫자 기록 실습

--- js/14_클릭한함수기록.js
+++ js/14_클릭한함수기록.js
@@ -4,6 +4,16 @@ const numbers = document.querySelectorAll(".number");
 const result = document.getElementById("result");
 const resetBtn = document.getElementById("resetBtn");
 
+// numbers[i].addEventListener("click", function () {
+//    result.textContent += this.textContent;     -> function을 사용할 때는 this 사용 가능, numbers[i]를 this로 대신 작성 가능
+// numbers[i].addEventListener("click", () => {
+//    result.textContent += e.target.textContent; -> function이 없기 때문에 this 사용 불가, addEventListener 앞 요소 명칭을 직접 작성해야 함
+// numbers[i].addEventListener("click", e => {
+//    result.textContent += e.target.textContent; -> function이 없기 때문에 this 사용 불가, 요소 명칭 대신 e.target 직접 작성해야 함
+// numbers[i].addEventListener("click", (e) => {
+//    result.textContent += e.target.textContent;
+
+/*
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function () {
     if (result.textContent.length == 10) {
@@ -11,6 +21,17 @@ for (let i = 0; i < numbers.length; i++) {
       return;
     }
     result.textContent += this.textContent;
+  });
+}
+*/
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", (e) => {
+    if (result.textContent.length == 10) {
+      alert("10글자까지만 입력 가능");
+      return;
+    }
+    result.textContent += e.target.textContent;
   });
 }
 
