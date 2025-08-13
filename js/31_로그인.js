be5b2ba705ad8ac40eb74cec5fe49eaff8b306c0
svv0003 <ssetoo4689@naver.com>
2025-08-13 10:18:22 +0900

    로그인, 로그아웃 유효 시간 설정

---
+++ js/31_로그인.js
@@ -0,0 +1,53 @@
+$(function(){
+  $("#login-btn").click(loginFn);
+})
+
+function loginFn(){
+  // localStorage에 저장된 회원가입 정보 가져오기
+  const storedUserJSON = localStorage.getItem("userInfo");
+
+  // 회원가입 정보가 존재하지 않으면 함수 종료
+  if(!storedUserJSON) {
+    alert("가입된 사용자 정보가 없습니다. 회원가입을 먼저 해주세요.");
+    return;
+  }
+
+  // 입력한 이메일 값을 html에서 js로 가져오기
+  const userEmail = $("#userEmail").val();
+
+  if (!userEmail) {
+    alert("이메일을 입력해 주세요.");
+    return;
+  }
+
+  // 저장된 정보(JSON 문자열)을 객체로 변환하기
+  const storedUser = JSON.parse(storedUserJSON);
+
+  // 입력한 이메일과 저장된 이메일을 비교하기
+  // 참고 : 추후 DB에 저장된 이메일 데이터와 사용자가 서버로 보낸 이메일의 일치 여부를 백엔드에서 작업 처리 진행한 뒤 프론트엔드로 전달한다.
+  //       연결된 DB가 존재하지 않기 때문에 프론트엔드에서 모든 작업 진행힌다.
+  if(userEmail === storedUser.email){
+    alert("로그인 성공!");
+
+
+  // 세션에 저장할 객체에 현재 로그인 시간(타임스탬프) 추가
+  const userSession = {
+    name: storedUser.name,
+    email: storedUser.email,
+    // new Data().getTime() : 현재 시간
+    loginTime: new Data().getTime(),
+  }
+  
+  // 웹사이트 session에 자바스크립트에서 작성한 로그인 정보를 저장한다.
+  sessionStorage.setItem("loggedInUser", JSON.stringify(userSession));
+
+  // 마이페이지로 이동, 현재 시간 확인하기
+  window.location.href = "31_마이페이지.html";
+  }
+
+  // 로그인 실패
+  else {
+    alert("이메일 정보가 일치하지 않습니다.");
+  }
+}
+
--- js/31_마이페이지.js
+++ js/31_마이페이지.js
@@ -1,4 +1,6 @@
 $(function(){
+    // 타이머를 중지시키기 위한 변수 설정
+    let timerInterval;
   // session Storage에서 로그인 정보 가져오기
   const sessionLoginUser = sessionStorage.getItem("loggedInUser");
 
@@ -9,15 +11,53 @@ $(function(){
     // if문 아래 기능들 실행 못하도록 돌려보내기
     return;
   }
+
   // 로그인 정보가 있으면 유저 정보를 문자열에서 객체로 변환하기
   let user = JSON.parse(sessionLoginUser);
   console.log("user : ", user);
+
+    // 타이머 및 자동 로그아웃 기능 시작
+    // 60000 = 1분(밀리초 단위)
+    const sessionDuration = 60000;
+
+    timerInterval = setInterval(function(){
+      // 현재 시간 생성하기
+      const now = new Date().getTime;
+      // session에 저장된 로그인 시간 가져오기
+      const loginTime = user.loginTime;
+      // 현재 시간 - 로그인한 시간 = 경과 시간
+      const elapsedTime = now - loginTime;
+      // 60000(1분) - 경과 시간 = 남은 시간
+      const remainingTime = sessionDuration - elapsedTime;
+
+      // 남은 시간 존재한다면 초 단위로 변환해서 화면에 표시하기
+      if (remainingTime > 0) {
+        // 남은 시간은 대부분 소수점 아래를 무조건 올림 함수를 사용한다.
+        // 예) 대중교통 예정 시간
+        const remainingSecond = Math.ceil(remainingTime/1000);
+        // #timer에 남은 시간 표시하기
+        $("#timer").text(remainingSecond);
+      }
+      // 시간이 만료되면 타이머 중지 및 로그아웃
+      else {
+        clearInterval(timerInterval);
+        sessionStorage.removeItem("loggedInUser");
+        alert("세션이 만료되어 자동으로 로그아웃 되었습니다.");
+        window.location.href = "31_로그인.html";
+      }
+      // 1초마다 반복 시행하기
+    },1000);
   
   // 화면에 사용자 정보 표시하기
   $("#user-name").text(user.name);
   $("#user-email").text(user.email);
 
-  $("#logout-btn").click(logoutFn);
+  $("#logout-btn").click(function(){
+    // 실행중인 타이머 중지
+    clearInterval(timerInterval);
+    // 로그아웃 기능 실행
+    logoutFn;
+  })
 })
 
 // 로그아웃
